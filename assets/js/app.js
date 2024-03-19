89e58e7aa8cc3d07e73eb43bc49403d61074c6e9
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-03-19 15:01:25 +0100

    [Completed] Now you can see the list on DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,12 +37,33 @@ createApp({
                 )
         },
 
+        // Genera una mail random
+        callApiRandomEmail() {
+            axios.
+                get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    console.log(response);
+                    console.log(response.data);
+                    this.myEmailList.push(response.data.response)
+                    console.log(this.myEmailList);
+                }
+                )
+        },
+
+
+
     },
 
 
     mounted() {
         this.callApiRandomSentence();
         this.callApiRandomInt();
+
+        //Genero una mail random per 10 volte
+        for (let i = 0; i < 10; i++) {
+            this.callApiRandomEmail();
+        }
+
     }
 
 
