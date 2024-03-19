ae7346b7da5ad036d9a8f67089aa0fff4a1c6fcc
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-03-19 14:47:12 +0100

    re-do morning snacks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,49 @@
-console.log("Ciaooooooooooooooooooooooooooo");
+//console.log("Ciaooooooooooooooooooooooooooo");
 
 const { createApp } = Vue
 createApp({
     data() {
         return {
+            randomSentence: null,
+            randomNumber: null,
             myEmailList: [],
-
         }
+    },
+
+    methods: {
+        // Morning test with Sentence
+        callApiRandomSentence() {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/sentence')
+                .then((response) => {
+                    console.log(response);
+                    console.log(response.data);
+                    this.randomSentence = response.data.response
+                    console.log(this.randomSentence);
+                }
+                )
+        },
+
+        // Morning test with Random numbers 0-9
+        callApiRandomInt() {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/int')
+                .then((response) => {
+                    console.log(response);
+                    console.log(response.data);
+                    this.randomNumber = response.data.response
+                    console.log(this.randomNumber);
+                }
+                )
+        },
+
+    },
+
+
+    mounted() {
+        this.callApiRandomSentence();
+        this.callApiRandomInt();
     }
 
-    
+
 }).mount('#app')
